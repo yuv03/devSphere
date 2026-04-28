bca2ab4db7f37a6c66a7dac48206b2722d9faa42
Yuvraj <yuvrajdholia@gmail.com>
2026-04-28 22:47:46 +0530

    Setting the query params

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@ const express = require("express");
 
 const app = express(); // here i am creating a express.js application
 
-app.use("/hello", (req, res) => {
-  res.send("hello hello helllo from the server");
+app.get("/user/:userId", (req, res) => {
+  console.log(req.params);
+  res.send({ firstName: "Yuv", lastName: "Dholia" });
 });
 
+// this will match all the HTTP method API call to /test
 app.use("/test", (req, res) => {
-  res.send("hello from the server");
-}); // this function is a request handler for a route.
+  res.send("Main Route handler");
+});
 
 app.listen(3000, () => {
   console.log("server is successfully listening on port 3000");
