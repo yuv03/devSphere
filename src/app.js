9685b8b619b7b8d191735bd48cfea4b39a8993ea
Yuvraj <yuvrajdholia@gmail.com>
2026-04-29 21:35:25 +0530

    Basic error handling

--- src/app.js
+++ src/app.js
@@ -3,19 +3,23 @@ const { adminAuth, userAuth } = require("./middlewares/auth");
 
 const app = express(); // here i am creating a express.js application
 
-app.use("/admin", adminAuth);
+app.get("/getUserData", (req, res) => {
+  // Suppose a logic of DB call and get user data
 
-app.get("/user", userAuth, (req, res) => {
-  res.send("All user data sent");
+  try {
+    throw new Error("asfsdfsd");
+    res.send("User data sent");
+  } catch (err) {
+    res.status(500).send("Something went wrong with the server");
+  }
+});
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong");
+  }
 });
 
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All data sent");
-});
-
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("Deleted the user");
-});
+// order of parameters in the handler of middleware matters
 
 app.listen(3000, () => {
   console.log("server is successfully listening on port 3000");
