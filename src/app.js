f13fef384eeed0647d046fc0fbbc94306c0a8d6b
Yuvraj <yuvrajdholia@gmail.com>
2026-04-29 21:05:53 +0530

    Added basic auth middleware

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
 const express = require("express");
+const { adminAuth, userAuth } = require("./middlewares/auth");
 
 const app = express(); // here i am creating a express.js application
 
-app.get("/user/:userId", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "Yuv", lastName: "Dholia" });
+app.use("/admin", adminAuth);
+
+app.get("/user", userAuth, (req, res) => {
+  res.send("All user data sent");
 });
 
-// this will match all the HTTP method API call to /test
-app.use("/test", (req, res) => {
-  res.send("Main Route handler");
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All data sent");
+});
+
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("Deleted the user");
 });
 
 app.listen(3000, () => {
---
+++ src/middlewares/auth.js
@@ -0,0 +1,23 @@
+const adminAuth = (req, res, next) => {
+  const token = "xyz";
+  const isAdminAuthorized = token === "xyz";
+  if (!isAdminAuthorized) {
+    res.status(401).send("Unauthorized request");
+  } else {
+    next();
+  }
+};
+
+const userAuth = (req, res, next) => {
+  const token = "xyz";
+  const isAdminAuthorized = token === "xyz";
+  if (!isAdminAuthorized) {
+    res.status(401).send("Unauthorized request");
+  } else {
+    next();
+  }
+};
+module.exports = {
+  adminAuth,
+  userAuth,
+};
