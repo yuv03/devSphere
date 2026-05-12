998cc7801c9634967cc2ed104917a5cd15b055db
Yuvraj <yuvrajdholia@gmail.com>
2026-05-12 22:36:26 +0530

    Created basic User Schema and initialized the database

--- src/app.js
+++ src/app.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const User = require("./models/User");
 
 const app = express(); // here i am creating a express.js application
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Yuv",
+    lastName: "Dholia",
+    emailId: "jsbfkjsdhjfk@abc.com",
+    password: "abc123",
+  });
+  // creating a new instance of the user model
+
+  await user.save();
+  try {
+    res.send("User added successfully");
+  } catch (err) {
+    res.status(400).send("Error in saving the user:" + err.message);
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database connection established...");
---
+++ src/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  emailId: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+  gender: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
