a7b80a059e359b233977d39d441e6070df2742da
Yuvraj <yuvrajdholia@gmail.com>
2026-05-14 23:09:26 +0530

    Created auth middleware for authenticating the routes

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const { userAuth } = require("./middlewares/auth");
 
 const app = express(); // here i am creating a express.js application
 app.use(express.json());
@@ -66,27 +67,10 @@ app.post("/login", async (req, res) => {
   }
 });
 
-app.get("/profile", async (req, res) => {
+app.get("/profile", userAuth, async (req, res) => {
   try {
-    const cookies = req.cookies;
-
-    const { token } = cookies;
-
-    if (!token) {
-      throw new Error("Invalid token");
-    }
-
-    // Validate the token
-    const decodedMessage = await jwt.verify(token, "DevSphere@%123");
-
-    console.log(decodedMessage);
-    const { _id } = decodedMessage;
-    const user = await User.findById(_id);
-
-    if (!user) {
-      throw new Error("User does not exist");
-    }
-
+    const { user } = req;
+    console.log("user: " + user);
     res.send(user);
   } catch (err) {
     res.status(400).send("ERROR: " + err.message);
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,23 +1,33 @@
-const adminAuth = (req, res, next) => {
-  const token = "xyz";
-  const isAdminAuthorized = token === "xyz";
-  if (!isAdminAuthorized) {
-    res.status(401).send("Unauthorized request");
-  } else {
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const userAuth = async (req, res, next) => {
+  try {
+    // Read the token from the req cookies
+
+    const { token } = req.cookies;
+
+    if (!token) {
+      throw new Error("Token missing");
+    }
+
+    // Validate the token
+    const decodedObj = await jwt.verify(token, "DevSphere@%123");
+    const { _id } = decodedObj;
+
+    // Find the user
+    const user = await User.findById(_id);
+
+    if (!user) {
+      throw new Error("User does not exist");
+    }
+    req.user = user;
     next();
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
   }
 };
 
-const userAuth = (req, res, next) => {
-  const token = "xyz";
-  const isAdminAuthorized = token === "xyz";
-  if (!isAdminAuthorized) {
-    res.status(401).send("Unauthorized request");
-  } else {
-    next();
-  }
-};
 module.exports = {
-  adminAuth,
   userAuth,
 };
