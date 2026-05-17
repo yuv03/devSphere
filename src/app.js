90b12e591146eeb3fc5b9c915434fc64c4830b8d
Yuvraj <yuvrajdholia@gmail.com>
2026-05-17 23:57:48 +0530

    Integrated the routers into the main app file

--- src/app.js
+++ src/app.js
@@ -16,6 +16,10 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestsRouter = require("./routes/requests");
 
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestsRouter);
+
 connectDB()
   .then(() => {
     console.log("Database connection established...");
