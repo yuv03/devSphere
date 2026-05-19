c28c0ac4f5351231a66148bb8927fb3b233ca38a
Yuvraj <yuvrajdholia@gmail.com>
2026-05-19 23:17:49 +0530

    Created the connection request API

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -14,7 +14,7 @@ const connectionRequestSchema = new mongoose.Schema(
       type: String,
       required: true,
       enum: {
-        values: ["ignore", "interested", "accepted", "rejected"],
+        values: ["ignored", "interested", "accepted", "rejected"],
         message: `{VALUE} is incorrect status type`,
       },
     },
--- src/routes/requests.js
+++ src/routes/requests.js
@@ -1,15 +1,37 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
 
 const requestRouter = express.Router();
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  try {
-    const user = req.user;
-    res.send(user.firstName + " sent the connection request");
-  } catch (err) {
-    res.status(400).send("ERROR: " + err.message);
-  }
-});
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id; //fromUserId is id of the logged in user ;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
+
+      const ALLOWED_STATUS = ["interested", "ignored"];
+
+      if (!ALLOWED_STATUS.includes(status)) {
+        res.status(400).json({ message: "Invalid user type" });
+      }
+
+      const ConnectionRequest = new ConnectionRequestModel({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      const connection = await ConnectionRequest.save();
+
+      res.json({ message: "Connection Request sent Successfully", connection });
+    } catch (err) {
+      res.status(400).send("ERROR: " + err.message);
+    }
+  },
+);
 
 module.exports = requestRouter;
